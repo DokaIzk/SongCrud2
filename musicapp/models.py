27e691bb9df3a1c2a3f5f2d3d1a7fe04c1ebaa5d
DokaIzk <edokaizkdaking247@gmail.com>
2022-11-05 00:18:26 +0100

    first commit

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,28 @@
+# from unittest.util import _MAX_LENGTH
 from django.db import models
+# from django.db import datetime
 
-# Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+    
+class Song(models.Model):
+    title = models.CharField(max_length=40)
+    released_date = models.DateTimeField()
+    likes = models.IntegerField()
+    artiste_id = models.IntegerField()
+    artistes = models.ForeignKey('Artiste', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=2000)
+    song_id = models.IntegerField()
+    songs = models.OneToOneField('Song', on_delete=models.CASCADE)
